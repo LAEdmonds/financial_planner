cf961110af4e7cf1b1b070d16cf51fe39b7a142c
Levi <leviaedmonds@gmail.com>
2025-04-24 11:33:50 -0400

    rudimentary gui to make sure it works. also now you can run the main.py to start the app

--- gui.py
+++ gui.py
@@ -1,12 +1,26 @@
 import customtkinter
 
-def button_callback():
-    print("Button clicked")
+class FinancialPlannerApp(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("800x800")
 
-app = customtkinter.CTk()
-app.geometry("400x150")
+        # Configure grid layout
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+        self.grid_columnconfigure((0,1), weight=1)
 
-button = customtkinter.CTkButton(app, text="My Button", command=button_callback)
-button.pack(padx=20, pady=20)
+        # Entry widget
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="Enter your income for this month")
+        self.entry.grid(row=0, column=0, padx=20, pady=20, sticky="w")
 
-app.mainloop()
+        # Button widget
+        self.button = customtkinter.CTkButton(self, text="Click Me", command=self.button_func)
+        self.button.grid(row=0, column=1, padx=20, pady=20, sticky="e")
+
+        # Textbox widget (initially not displayed, will be added on button click)
+        self.textbox = customtkinter.CTkTextbox(self, width=400, corner_radius=0)
+
+    def button_func(self):
+        text = self.entry.get()
+        self.textbox.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")  # Use grid to place textbox
+        self.textbox.insert("0.0", text)
